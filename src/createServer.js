ed2c05f58159651d272334d15a5728d9f7b432b9
Daria Nastas <bycinska@gmail.com>
2024-12-29 14:49:49 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,27 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    const parts = parsedUrl.pathname.split('/').filter((part) => part !== '');
+
+    const query = Object.fromEntries(parsedUrl.searchParams.entries());
+
+    const response = {
+      parts: parts,
+      query: query,
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(JSON.stringify(response));
+  });
+
+  return server;
 }
 
 module.exports = {
